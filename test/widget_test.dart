a89bfec560d12e2944c5095a0c3d6f6be3187ec9
charleston <charles1235223@gmail.com>
2023-08-21 11:37:09 -0400

    feat - add module home and home page initial

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:imc_challenge/main.dart';
+import 'package:imc_challenge/src/imc_challenge.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
