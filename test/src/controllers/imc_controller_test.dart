38970dffd39be6a47c7ff6c0ffccd63ff45690ed
charleston <charles1235223@gmail.com>
2023-08-22 17:11:12 -0400

    test - Interprete imc service success

---
+++ test/src/controllers/imc_controller_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:imc_challenge/src/controllers/imc_controller.dart';
+import 'package:imc_challenge/src/models/person_model.dart';
+
+final imcService = ImcService();
+final personModel = PersonModel(name: 'Charles', weight: 80, height: 1.70);
+
+void main() {
+  test('Calculate imc service success...', () async {
+    final result = imcService.calculateIMC(personModel: personModel);
+    expect(result, equals(27.7));
+  });
+
+  test('Interprete imc service success...', () async {
+    final imc = imcService.calculateIMC(personModel: personModel);
+    final result = imcService.interpretIMC(imc: imc, name: 'Charles');
+    expect(result, equals('IMC: 27.7. Charles vc está com sobrepeso.'));
+  });
+}
